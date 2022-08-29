6e53f48b2d87f64f408cb96bf5351d2f9b8e2b15
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-08-30 01:13:27 +0200

    add run model scripts

---
+++ people-counter/scripts/run_default.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+export DISPLAY=172.25.16.1:0.0 && . ../.venv/bin/activate && python app.py infer | ffmpeg -v warning -f rawvideo -pixel_format rgb24 -video_size 1280x720 -framerate 24 -i - http://ffserver:3005/fac.ffm
