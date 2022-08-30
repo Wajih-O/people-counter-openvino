e13bdb10865aeed62d4dcb702da471b55c8d0cb0
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-08-30 08:12:13 +0200

    update models download scripts

--- people-counter/scripts/download_models_from_container.sh
+++ people-counter/scripts/download_models_from_container.sh
@@ -4,6 +4,6 @@ if [ "$#" -eq 1 ];
 then
     # TODO test if the models file exist
     echo "downlod models from $1"
-    cat $1 | while read model;do omz_downloader --name $model -o models ; done
+    . ../.venv/bin/activate && cat $1 | while read model;do omz_downloader --name $model -o models ; done
 fi
 
