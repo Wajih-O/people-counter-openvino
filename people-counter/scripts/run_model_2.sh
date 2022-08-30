083a0f202201558dbce0fe3b2a82632ace735422
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-08-30 07:57:19 +0200

    remove export DISPLAY (already passedthough with docker-compose/Dockerfile)

--- people-counter/scripts/run_model_2.sh
+++ people-counter/scripts/run_model_2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-export DISPLAY=172.25.16.1:0.0 && . ../.venv/bin/activate && python app.py infer --model="person-detection-0201" | ffmpeg -v warning -f rawvideo -pixel_format rgb24 -video_size 1280x720 -framerate 24 -i - http://ffserver:3005/fac.ffm
+. ../.venv/bin/activate && python app.py infer --model="person-detection-0201" | ffmpeg -v warning -f rawvideo -pixel_format rgb24 -video_size 1280x720 -framerate 24 -i - http://ffserver:3005/fac.ffm
